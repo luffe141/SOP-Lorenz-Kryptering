bb2c3aad9fae0c8d047a796d0844b0f3e9a44a44
jonathan johansen <63632617+luffe141@users.noreply.github.com>
2020-12-17 16:19:13 +0100

    Add files via upload

---
+++ snippet (3).py
@@ -0,0 +1,48 @@
+K = 'abcdefg'
+S = 'opqrs'
+
+msg = input("Input message")
+startK = input("Start position, K: ")
+startS = input("Start position, S: ")
+
+#################################
+through_K = ''
+for letter in msg:
+	a=startK%len(K)
+	through_K += XOR(letter, K[a])
+	startK += 1
+
+#################################
+cipher = ''
+for item in through_K:
+	a=startS%len(S)
+	cipher += XOR(item, S[a])
+    if doRotate(M):
+        startS += 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
