6be849306b5fb861092762f7a7575e336959fe58
jonathan johansen <63632617+luffe141@users.noreply.github.com>
2020-12-17 16:20:45 +0100

    Update and rename snippet (3).py to Lorenz.py

--- Lorenz.py
+++ Lorenz.py
@@ -5,14 +5,13 @@ msg = input("Input message")
 startK = input("Start position, K: ")
 startS = input("Start position, S: ")
 
-#################################
+
 through_K = ''
 for letter in msg:
 	a=startK%len(K)
 	through_K += XOR(letter, K[a])
 	startK += 1
 
-#################################
 cipher = ''
 for item in through_K:
 	a=startS%len(S)
